e17e992960fcbb71b6dc275d10c747b7a7bc23f0
Freddy1303 <flopezparia@gmail.com>
2024-03-22 19:06:24 -0500

    otra forma

--- js/script.js
+++ js/script.js
@@ -24,18 +24,7 @@ $('#contactForm').submit(function(event) {
                 }).then((result) => {
                     // Si se hace clic en el botón "OK", recargar la página
                     if (result.isConfirmed) {
-                        document.getElementById("contactForm").addEventListener("submit", function(event) {
-
-                            event.preventDefault();
-                    
-                            // Limpia los valores de los campos del formulario
-                            document.getElementById("nombre").value = "";
-                            document.getElementById("email").value = "";
-                            document.getElementById("asunto").value = "";
-                            document.getElementById("mensaje").value = "";
-                            document.getElementById("adjunto").value = "";
-                    
-                        });
+                        document.getElementById("contactForm").reset();
                     }
                 });
             } else {
